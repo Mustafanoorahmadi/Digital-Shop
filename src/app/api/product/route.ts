ea76a1ef64d27f4c1008b0903fa288d6ed7a454c
Mostafa <noorahmadimustafa204@gmail.com>
2025-04-15 18:09:30 +0430

    add : GET endpoint to fetch products with images

---
+++ src/app/api/product/route.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  console.log(req);
+
+  const result = await prisma.product.findMany({ include: { images: true } });
+
+  return NextResponse.json({
+    data: result, 
+  });
+}
