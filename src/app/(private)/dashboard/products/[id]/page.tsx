c10e8dc56761117e5ef02035997f41ba154edc40
Mostafa <noorahmadimustafa204@gmail.com>
2025-04-23 21:50:44 +0430

    Add: add error, loading, and not-found components for improved error handling and UX

--- src/app/(private)/dashboard/products/[id]/page.tsx
+++ src/app/(private)/dashboard/products/[id]/page.tsx
@@ -1,12 +1,16 @@
-import ProductDetailView from '@/modules/products/views/ProductDetailView';
-import React, { Suspense } from 'react';
+import Spinner from "@/components/Spinner";
+import ProductDetailView from "@/modules/products/views/ProductDetailView";
+import React, { Suspense } from "react";
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const data = await params;
   const { id } = data;
   return (
     <div>
+      some other components...
+      <Suspense fallback={<Spinner />}>
         <ProductDetailView id={id} />
+      </Suspense>
     </div>
   );
 };
