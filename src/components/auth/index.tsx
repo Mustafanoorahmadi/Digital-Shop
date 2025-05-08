43c32312c5097ca22114bb0560cc2aefc090e3ba
Mostafa <noorahmadimustafa204@gmail.com>
2025-05-08 14:24:13 +0430

    feat: add admin route access control

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,4 +1,3 @@
-
 import { currentUser } from "@clerk/nextjs/server";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import AdminMenu from "./AdminMenu";
@@ -9,7 +8,24 @@ async function Auth() {
   const isAdmin = user?.privateMetadata?.isAdmin;
   return (
     <div>
-      <SignedIn>{isAdmin ? <AdminMenu /> : <UserButton />}</SignedIn>
+      <SignedIn>
+        {isAdmin ? (
+          <AdminMenu />
+        ) : (
+          <UserButton
+            appearance={{
+              elements: {
+                userButtonPopoverCard:
+                  "bg-white shadow-lg rounded-xl border border-gray-200",
+                userButtonPopoverFooter: "hidden", // hides the "Powered by Clerk" footer
+                userButtonAvatarBox: "ring-2 ring-indigo-500", // adds ring to avatar
+                userButtonPopoverActionButton:
+                  "text-sm hover:bg-gray-100 text-gray-800",
+              },
+            }}
+          />
+        )}
+      </SignedIn>
       <SignedOut>
         <SignInButton />
       </SignedOut>
